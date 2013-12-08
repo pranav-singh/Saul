6714e9db3f110717150d5abb515874542ba55919
Pranav Singh <singh.pranavkumar10@gmail.com>
2013-12-09 00:22:06 +0530

    Extracted all the problems

--- rec.py
+++ rec.py
@@ -6,7 +6,23 @@ html  = urllib2.urlopen(url).read()
 content = []
 content = html.split('\n')
 for line in content:
-  res = re.match('<a href="/problems/(\w+).*</a></td>$',line)
+  res = re.match('<a.*start=(\d+)',line)
   if res:
-    print res.group(1)
+    total = int(res.group(1))
+
+f = open('problems','a')
+num = 0
+
+while num <= total:
+  for line in content:
+    res = re.match('<a href="/problems/(\w+).*</a></td>$',line)
+    if res:
+      f.write(res.group(1))
+      f.write("\n")
+  num = num + 50
+  url = "http://www.spoj.com/problems/classical/start=" + str(num)
+  html  = urllib2.urlopen(url).read()
+  content = html.split('\n')
+
+
 
